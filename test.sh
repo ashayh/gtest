d76820261603139434d511179f063798d340da6f
Ashay Humane <ashay.humane@gmail.com>
2017-11-27 16:10:25 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+set -u
 
-exit 0
+COUNT=0
+
+while true
+do
+  echo $COUNT
+  echo $COUNT | logger -t $0
+  sleep 1
+  ((COUNT++))
+  if [[ ${COUNT} -gt 300 ]] ; then
+    exit
+  fi
+done
